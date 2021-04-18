97a608c4c421650611dd4ffa7fba81f347a1a830
Aigamo <51428094+ycanardeau@users.noreply.github.com>
2021-04-18 20:51:44 +1000

    Update HomeController.nicoRegex

--- NicoDataExtractor/Controllers/HomeController.cs
+++ NicoDataExtractor/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace NicoDataExtractor.Controllers
 	public class HomeController : Controller
 	{
 		private static readonly HttpClient client = new HttpClient();
-		private readonly Regex nicoRegex = new Regex(@"^https?://www\.nicovideo\.jp/watch/((?:sm|nm|so)\d+)");
+		private readonly Regex nicoRegex = new Regex(@"^https?://www\.nicovideo\.jp/watch/(\w+)");
 
 		public async Task<IActionResult> Index(string nicoUrl = null)
 		{
